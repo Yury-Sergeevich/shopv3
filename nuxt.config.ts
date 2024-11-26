2e32e6e955cce72525f65d33374c22bce282574c
Yury-Sergeevich <yury.kravchenko.1987@gmail.com>
2024-11-26 23:22:35 +0300

    Added options in the project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  css: ["@/assets/scss/main.scss"],
-  modules: ["@nuxtjs/color-mode", "nuxt-icons"],
+  ssr: true,
+  css: ['@/assets/scss/main.scss'],
+  modules: [
+    '@nuxtjs/color-mode',
+    'nuxt-icons',
+    '@nuxt/eslint',
+    '@nuxtjs/web-vitals',
+    '@vite-pwa/nuxt',
+    '@nuxt/image',
+    "@nuxtjs/robots', '@nuxtjs/sitemap",
+  ],
+
   colorMode: {
-    classSuffix: "",
-    preference: "system",
-    fallback: "light",
+    classSuffix: '',
+    preference: 'system',
+    fallback: 'light',
   },
 });
